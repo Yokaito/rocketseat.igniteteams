1ea4fe88aee6055d851e47d8a2214a30790dbc82
Guilherme Fontes <gui.fontes.amorim@gmail.com>
2023-03-21 22:15:34 -0400

    feat: add assets and path modules

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+export const Groups = () => {
+  return (
+    <View>
+      <Text>Guilherme Fontes</Text>
+    </View>
+  );
+};
+
+export default Groups;
